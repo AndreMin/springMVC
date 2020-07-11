5a900984efb48be69b034d59b9aaab85ad26e8d8
AndreMin <minok.mal@gmail.com>
2020-07-11 12:36:54 +0300

    applying validations @InitBinder

--- src/mvcpackage/CustomerController.java
+++ src/mvcpackage/CustomerController.java
@@ -1,9 +1,12 @@
 package mvcpackage;
 
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,13 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+//    add initbinder to covert trim input strings
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+       dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @RequestMapping("/showForm")
     public String showForm(Model model){
         model.addAttribute("customer", new Customer());
@@ -20,6 +30,7 @@ public class CustomerController {
 
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute ("customer") Customer customer, BindingResult bindingResult){
+        System.out.println("Last name: |"+customer.getLastName()+"|");
        if(bindingResult.hasErrors()){
            return "customer-form";
        }else {
